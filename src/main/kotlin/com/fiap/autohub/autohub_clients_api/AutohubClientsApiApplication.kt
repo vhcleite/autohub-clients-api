c44992e7470cc9c467b6ecb6190cc46ded7bcf1c
Victor Leite <victor_leite@outlook.com.br>
2025-04-04 13:51:10 -0300

    initial commit

---
+++ src/main/kotlin/com/fiap/autohub/autohub_clients_api/AutohubClientsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.fiap.autohub.autohub_clients_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutohubClientsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutohubClientsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/fiap/autohub/autohub_clients_api/AutohubClientsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fiap.autohub.autohub_clients_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutohubClientsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
